d505443e35792aa4641c4930c7fbf8ba804966b1
asadsanto10 <santo.meridian@gmail.com>
2020-09-29 17:31:02 +0600

    update

--- contact.php
+++ contact.php
@@ -2,9 +2,9 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
-        $to = 'santo.meridian@gmail.com'; // Your e-mail address here.
+        $to = 'connect@feenixlab.com'; // Your e-mail address here.
         $body = "\nName: {$_POST['contactname']}\nEmail: {$_POST['contactemail']}\nPhone: {$_POST['contactphone']}\n\n{$_POST['contactmessage']}\n\n";
-        mail($to, "Message from Kinfozone Inc.", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+        mail($to, "Message from Feennix Lab", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
     }
 }
 ?>
